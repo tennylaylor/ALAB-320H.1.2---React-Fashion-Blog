71216237caad6be21529a9acece75ed0126c47b2
tennylaylor <Lennyttaylor@gmail.com>
2024-12-18 23:59:41 -0500

    update app.jsx

--- React-Fashion-Blog/reactfashion/src/components/App.jsx
+++ React-Fashion-Blog/reactfashion/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
-import Article from "./components/Article";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Article from "../components/Article";
+import Footer from "../components/Footer";
 
 const App = () => {
   return (
